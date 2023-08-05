6d41c6492a2489a5512ac56b732a7263401218bc
iker <A01749675@tec.mx>
2023-08-05 16:21:28 -0600

    Logic update on the go_to function

--- scripts/route.py
+++ scripts/route.py
@@ -2,6 +2,7 @@
 import numpy as np
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 from navigationpuebla.msg import odom
 import time
 import consts as const
@@ -12,6 +13,7 @@ class Route():
         rospy.init_node("Route",anonymous=True)
         self.twist = Twist()
         self.pub_cmd = rospy.Publisher("/cmd_vel",Twist,queue_size=10)
+        self.pub_go_to = rospy.Publisher("/go_to",String,queue_size=10)
         self.listener_odom = rospy.Subscriber("/odom",odom,self.callback)
         self.start_time = rospy.get_time()
         self.rate= rospy.Rate(1)
@@ -21,6 +23,7 @@ class Route():
         self.velocity=0.33
         self.angular_velocity = 0.4
         self.coordinates = []
+        self.arrived = False
     
     def callback(self,data):
         self.x=data.x
@@ -33,7 +36,7 @@ class Route():
     def go_to(self,x1,y1):
         cuadrante = 0
         distance = np.sqrt(pow(x1-self.x,2)+pow(self.y-y1,2))
-        angle = np.arctan(abs(self.x-x1),abs(self.y-y1))
+        angle = np.arctan(abs(self.y-y1)/(abs(self.x-x1)))
         
         if(x1>self.x):
             if(y1>self.y):
@@ -55,24 +58,32 @@ class Route():
             while(self.theta>angle):
                 self.twist.linear.x=0
                 self.twist.angular.z=self.angular_velocity
-                self.pub_cmd(self.twist)
+                self.pub_cmd.publish(self.twist)
         else:
             while(self.theta<angle):
                 self.twist.linear.x=0
                 self.twist.angular.z=self.angular_velocity
-                self.pub_cmd(self.twist)
+                self.pub_cmd.publish(self.twist)
 
         target_time = distance/self.velocity+rospy.get_time()
 
         while(target_time>rospy.get_time()):
-            self.twist.linear.x=self.velocity/(target_time-rospy.get_time())
+            self.twist.linear.x=self.velocity
             self.twist.angular.z=0
-            self.pub_cmd(self.twist)
+            self.pub_cmd.publish(self.twist)
+            if(self.x >x1-const.POSITION_ERROR and self.x<x1+const.POSITION_ERROR):
+                if(self.y>y1-const.POSITION_ERROR and self.y<y1+const.POSITION_ERROR):
+                    break
 
-    
+        self.pub_go_to.publish("Arrived")
+        self.arrived=True
+        self.twist.linear.x=0
+        self.twist.angular.z=0
+        self.pub_cmd.publish(self.twist)
     def main(self):
         while not rospy.is_shutdown():
-            self.routine()
+            self.go_to(5,5)
+            break
             self.rate.sleep()
     
 
