821db103c2e3687ddba9d3584c2ebf5ecb155e19
iker <A01749675@tec.mx>
2023-08-15 17:17:17 -0600

    logic

--- scripts/route.py
+++ scripts/route.py
@@ -77,7 +77,7 @@ class Route():
         print("Target angle: ",angle)
         if(self.theta>angle):
             print("-Moving from angle ",self.theta, " to ",angle)
-            self.angular_velocity = -self.angular_velocity
+            #self.angular_velocity = -self.angular_velocity
             while(self.theta>angle*const.ODOM_ANGLE_CORRECTION):
                 if(self.theta-const.ODOM_ANGLE_ERROR<angle):
                     self.twist.linear.x=0.0
@@ -90,7 +90,7 @@ class Route():
                     self.pub_cmd.publish(self.twist)
         else:
             print("+Moving from angle",self.theta, " to ",angle)
-            #self.angular_velocity = -self.angular_velocity
+            self.angular_velocity = -self.angular_velocity
             while(self.theta<angle*const.ODOM_ANGLE_CORRECTION): 
                 if(self.theta+const.ODOM_ANGLE_ERROR>angle):
                     self.twist.linear.x=0
