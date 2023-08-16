fcd2e15e1372656bd1cfacfa18a6583d6441c171
Cesar Flores <A01751101@tec.mx>
2023-08-15 22:11:26 -0600

    Valores y servos de prueba approach_lab

--- scripts/approach_lab.py
+++ scripts/approach_lab.py
@@ -1,9 +1,52 @@
 #!/usr/bin/env python3
 
 import rospy
-import time as tm
+import time
 from std_msgs.msg import Int32
 
 servoRight = rospy.Publisher("servo_right",Int32,queue_size=1)
 servoCenter = rospy.Publisher("servo_center",Int32,queue_size=1)
 servoLeft = rospy.Publisher("servo_left",Int32,queue_size=1)
+
+while not rospy.is_shutdown():
+    print("Home")
+    servoRight.publish(0)
+    servoCenter.publish(130)
+    servoLeft.publish(300)
+
+    time.sleep(10)
+
+    print("Intermediate")
+    servoRight.publish(0)
+    servoCenter.publish(65)
+    servoLeft.publish(358)
+
+    time.sleep(10)
+
+    print("Ground") #Cuidado que se mata
+    servoRight.publish(0)
+    servoCenter.publish(-24)
+    servoLeft.publish(430)
+
+    time.sleep(10)
+
+    print("Intermediate")
+    servoRight.publish(0)
+    servoCenter.publish(65)
+    servoLeft.publish(358)
+
+    time.sleep(10)
+
+    print("Ground Ex") #Probar en la lap de chevez con
+    servoRight.publish(0) #valores de ground normal
+    servoCenter.publish(-24)
+    servoLeft.publish(430)
+
+    time.sleep(10)
+
+    print("Intermediate")
+    servoRight.publish(0)
+    servoCenter.publish(65)
+    servoLeft.publish(358)
+
+    time.sleep(10)
