9182451358c166fe16b00cab776c4c914d3402c1
A01751101 <A01751101@tec.mx>
2023-08-30 13:19:33 -0500

    odometry sub 100%, cmd_vel sub aún no

--- scripts/PygameSim.py
+++ scripts/PygameSim.py
@@ -7,22 +7,25 @@ class PygameSim():
     def __init__(self):
         rospy.init_node("Pygame",anonymous=True)
         rospy.Subscriber("/odometry",Pose2D,self.callback)
-        self.odometry = Pose2D()
-        self.rate= rospy.Rate(10)
+        rospy.Subscriber("/cmd_vel",Twist,self.callback)
         self.x=0.0
         self.y=0.0
         self.theta=0.0
+        #Cmd_vel aún no implmentado
+        self.vx=0.0
+        self.vy=0.0
+        self.vTheta=0.0
 
     def callback(self,data):
         self.x=data.x
         self.y=data.y
         self.theta=data.theta
 
-    def main(self):
-        print("hola1")
+    def main(self): #este es el main
         while not rospy.is_shutdown():
-            print("hola2")
-            print(self.x," ",self.y)
+            x = float(self.x) #pose en x
+            y = float(self.y) #pose en y
+            theta = float(self.theta) #angulo
 
 if __name__=="__main__":
     pygame = PygameSim()
